32bf91db822aacdc79d19002cb7725ce4b24b8c5
tubstrr <me@jonknoll.dev>
2024-08-24 18:42:57 -0500

    update: x-if rather than x-show

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -6,7 +6,7 @@
     @endif
 
     <div class="container" x-data="{ loading: $wire.entangle('loading') }">
-        <div x-show="loading" class="loading">Loading...</div>
+        <div x-if="loading" class="loading">Loading...</div>
         <ul>
             @foreach ($todos as $todo)
                 <li>
@@ -33,25 +33,6 @@
                 </li>
             @endforeach
         </ul>
-        {{-- Nuxt3 <form @submit.prevent="addTodos">
-          <div>
-            <label for="addNew">Add New Item:</label>
-            <input type="text" v-model="newTodo" id="addNew" />
-          </div>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 448 512"
-            >
-              <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-              <path
-                d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
-              />
-            </svg>
-            Add
-          </button>
-        </form> --}}
 
         <form @submit="loading = true;" wire:submit="add">
             <div>
